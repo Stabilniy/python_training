3f3bde518c0a4032da682c324310ab4959b0fb1b
Stabilniy <32289580+Stabilniy@users.noreply.github.com>
2020-04-29 15:27:05 +0300

    Second_hometask

--- test_add_group.py
+++ test_add_group.py
@@ -13,26 +13,26 @@ class TestAddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
+
     def test_add_group(self):
         wd = self.wd
         self.open_homepage()
-        self.login(wd)
+        self.login(wd, username = "admin", password = "secret")
         self.open_groups_page(wd)
         self.create_group(wd, Group(name = "testname", header = "testheader", footer = "testfooter"))
         self.return_to_group_page(wd)
         self.logout(wd)
 
+
     def logout(self, wd):
-        # logout
         wd.find_element_by_link_text("Logout").click()
 
+
     def return_to_group_page(self, wd):
-        # return to the group page
         wd.find_element_by_link_text("group page").click()
 
+
     def create_group(self, wd, group):
-        # create group
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -44,21 +44,21 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("submit").click()
         wd.find_element_by_xpath("//div[@id='content']/div").click()
 
+
     def open_groups_page(self, wd):
-        # open group page
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
-        # login
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
+
     def open_homepage(self):
-        # open_homepage
         wd = self.wd
         wd.get("http://localhost/addressbook/")
         return wd
