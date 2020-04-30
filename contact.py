0a52fee26c9b3219bdac9a44c5b46c2f4a374fa6
Stabilniy <32289580+Stabilniy@users.noreply.github.com>
2020-04-30 12:14:12 +0300

    Third hometask

---
+++ contact.py
@@ -0,0 +1,30 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname , nickname, title, company, address, home, mobile, work, fax, email, email1, email2, email3,
+                 address2, homepage, bday, bmounth, byear, aday, amounth, ayear, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.address2 = address2
+        self.homepage = homepage
+        self.bday = bday
+        self.bmounth = bmounth
+        self.byear = byear
+        self.aday = aday
+        self.amounth = amounth
+        self.ayear = ayear
+        self.phone2 = phone2
+        self.notes = notes
+
+
